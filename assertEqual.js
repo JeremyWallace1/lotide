72aaff221ad2e0d63bfd6ba654bf467251f541bd
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-14 14:18:39 -0700

    assertEqual created with emojis for quick readability and more verbose text

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "1");
+assertEqual(5, "five");
+assertEqual(true, (5 === 5));
