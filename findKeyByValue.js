b852f96c93ae83fc7768cd1dd9720982a792db51
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-16 15:09:13 -0700

    created and solved findKeyByValue and used AssertEqual to confirm working properly.

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  // scans the object and returns the first key which contains the given value
+  // if no key with that given value is found, then it should return undefined
+  for (key in object) {
+    if (value === object[key]) {
+      return key;
+    }
+  }
+  return;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
