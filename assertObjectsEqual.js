a9f055583f871fa07e92d0dca6d6d3f7e4edd79f
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-16 16:45:31 -0700

    cleaned up assertObjectsEqual.js by removing old function assertEqual that wasn't being used

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let comparison = (array1 === array2);
   
