4baedc93d4e4b66d6707867728fe66fff277b62e
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-18 15:51:37 -0700

    refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -2,19 +2,18 @@ const eqArrays = function(array1, array2) {
   let comparison = (array1 === array2);
   
   if (!array1.length && !array2.length && Array.isArray(array1) && Array.isArray(array2)) { //captures empty arrays
-    comparison = true;
+    return false;
   } else if (array1.length !== array2.length) {
-    comparison = false;
+    return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
-      comparison = (array1[i] === array2[i]);
-      if (comparison === false) {
-        break;
+      if (array1[i] !== array2[i]) {
+        return false;
       }
     }
   }
   //console.log (`array1 is ${array1.length} items long.\narray2 is ${array2.length} items long.\narray1 is ${array1}\narray2 is ${array2}\nThe comparison is ${comparison}`);
-  return comparison;
+  return true;
 };
 
 const assertEqual = function(actual, expected) {
@@ -26,11 +25,13 @@ const assertEqual = function(actual, expected) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => should FAIL
 assertEqual(eqArrays([1, 2, 3, 4, "x"], [1, 2, 3, 4, "x"]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4, "x"], [1, 2, 3, 4]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3, 4, "x"], [1, 2, 3, 4]), false); // => should FAIL
+assertEqual(eqArrays([], []), false); // => should FAIL
+assertEqual(eqArrays([1, 2], "monkey"), false); // => should FAIL
