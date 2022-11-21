18dede4e2519dd6a926bb58a3d247f3382e92df6
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 09:52:02 -0700

    refactored assertArraysEqual.js to require eqArrays instead of duplicating code and to export itself. pushed testing code into test/assertArraysEqualTest.js and imported assertArraysEqual.js there

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqArrays(actual, expected)) {
@@ -7,31 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  let comparison = (array1 === array2);
-  
-  if (!array1.length && !array2.length && Array.isArray(array1) && Array.isArray(array2)) { //captures empty arrays
-    comparison = true;
-  } else if (array1.length !== array2.length) {
-    comparison = false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      comparison = (array1[i] === array2[i]);
-      if (comparison === false) {
-        break;
-      }
-    }
-  }
-  //console.log (`array1 is ${array1.length} items long.\narray2 is ${array2.length} items long.\narray1 is ${array1}\narray2 is ${array2}\nThe comparison is ${comparison}`);
-  return comparison;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
-
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => should FAIL
-assertArraysEqual([1, 2, 3, 4, "x"], [1, 2, 3, 4, "x"]); // => should PASS
-assertArraysEqual([1, 2, 3, 4, "x"], [1, 2, 3, 4]); // => should FAIL
+module.exports = assertArraysEqual; //export the function
