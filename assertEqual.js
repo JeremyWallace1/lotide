dd05c42356ab242b25aee2b6632cb61887f8c9d1
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 09:14:25 -0700

    add code to export the function from this module

--- assertEqual.js
+++ assertEqual.js
@@ -6,6 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertEqual; //export the function
+
+/* 
+NB: Notice how we are passing the variable assertEqual (which points to our function) and not calling assertEqual via assertEqual() here. This is an important distinction.
+*/
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, "1");
