c63e30a666adaf36e333f8fdeb24591caad7ff18
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 10:59:37 -0700

    removed commented out tests for middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,3 @@
-// assertEqual(eqArrays(middle([1]), []), false);
-// assertEqual(eqArrays(middle([1, 2]), []), false);
-// assertEqual(eqArrays(middle([1, 2, 3]), [2]), true);
-// assertEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
-// assertEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
-// assertEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
-
 const { assert } = require('chai');
 const middle = require('../middle');
 
