cca083a2aeddb413b36885889079d5a76e206a5f
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 09:25:48 -0700

    removed require assertEqual from head.js as it's for testing so it is required in headTest, not head.js

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(array) { // The head function should not return the first element as an array. It should simply return the element itself.
   return array[0];
 };
