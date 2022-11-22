af55b030fe848d32758dd96524b1a39b3913a55a
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-21 18:05:08 -0700

    removed space after it multiple times on eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,28 +5,28 @@ describe("#eqArrays", () => {
   it("returns true if [1, 2, 3], [1, 2, 3] are equal", () => {
     assert.equal(_.eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
   });
-  it ("returns false if 1, 2, 3], [3, 2, 1] are not equal", () => {
+  it("returns false if 1, 2, 3], [3, 2, 1] are not equal", () => {
     assert.equal(_.eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
   });
-  it ('returns true if ["1", "2", "3"], ["1", "2", "3"] are equal', () => {
+  it('returns true if ["1", "2", "3"], ["1", "2", "3"] are equal', () => {
     assert.equal(_.eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
   });
-  it ('returns false if ["1", "2", "3"], ["1", "2", 3] are not equal', () => {
+  it('returns false if ["1", "2", "3"], ["1", "2", 3] are not equal', () => {
     assert.equal(_.eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
   });
   it("returns false if [1, 2, 3], [1, 2, 3, 4] are not equal", () => {
     assert.equal(_.eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // => should FAIL
   });
-  it ('returns true if [1, 2, 3, 4, "x"], [1, 2, 3, 4, "x"] are equal', () => {
+  it('returns true if [1, 2, 3, 4, "x"], [1, 2, 3, 4, "x"] are equal', () => {
     assert.equal(_.eqArrays([1, 2, 3, 4, "x"], [1, 2, 3, 4, "x"]), true); // => should PASS
   });
-  it ('returns false if [1, 2, 3, 4, "x"], [1, 2, 3, 4] are not equal', () => {
+  it('returns false if [1, 2, 3, 4, "x"], [1, 2, 3, 4] are not equal', () => {
     assert.equal(_.eqArrays([1, 2, 3, 4, "x"], [1, 2, 3, 4]), false); // => should FAIL
   });
-  it ('returns false if [1, 2], "monkey" are not equal', () => {
+  it('returns false if [1, 2], "monkey" are not equal', () => {
     assert.equal(_.eqArrays([1, 2], "monkey"), false); // => should FAIL
   });
-  it ("returns false if [], [] are not equal", () => {
+  it("returns false if [], [] are not equal", () => {
     assert.equal(_.eqArrays([], []), false); // => should FAIL
   });
 });
